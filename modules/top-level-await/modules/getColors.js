6271bdd927dde0528277806f4e82fa9b246a4b9e
Rumyra <rumyra@gmail.com>
2021-01-25 14:01:35 +0000

    fixed issue

--- modules/top-level-await/modules/getColors.js
+++ modules/top-level-await/modules/getColors.js
@@ -1,5 +1,5 @@
 // fetch request
-const colors = fetch('../data/colors.json')
+const colors = fetch('/modules/top-level-await/data/colors.json')
 	.then(response => response.json());
 
 export default await colors;
